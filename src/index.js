70df1a36b4b81a0f03299c866011d9e0379908e8
Daniiar Oskonbaev <daniiar94@gmail.com>
2021-03-29 19:17:18 +0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,67 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./style.css"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// const name = "Daniiar"
+// const age = "32"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+// const app = (
+//     <h1 className="title">My name is {name}, I am {age} years old
+//     </h1>
+// )
+
+const Text = () => {
+
+    const text = "lorem"
+    return (<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    Ut, culpa quae quidem expedita hic totam. Inventore cupiditate
+    eum est! Aliquam beatae minus maxime expedita corrupti libero modi
+         rerum natus possimus?</p>)
+}
+
+const Header = () => {
+    
+    return (
+    <div id="myDIV" className="header">
+        <h2>My To Do List</h2>
+        <input type="text" id="myInput" placeholder="Title..." />
+        <span className="addBtn">Add</span>
+    </div>)
+}
+
+const Body = () => {
+    
+    return ( <ul id="myUL">
+        <li>Hit the gym</li>
+        <li className="checked">Pay bills</li>
+        <li>Meet George</li>
+        <li>Buy eggs</li>
+        <li>Read a book</li>
+        <li>Organize office</li>
+    </ul>
+    )
+}
+
+
+const App = () => {
+
+    // const name = "Daniiar"
+    // const age = "32"
+
+    return (
+        <div>
+            <Header/>
+            <Body/>
+        </div>
+    )
+}
+// бул функция бирок реакт жс те компонент деп аталат
+
+
+ReactDOM.render(<App />, document.querySelector("#root"))
+
+
+
+
